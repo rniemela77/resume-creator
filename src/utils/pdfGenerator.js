bcbca4dcbd042d7600c870e9ef584932393ff9fe
rniemela77 <rvniemela@hotmail.com>
2025-06-18 14:22:59 -0700

    Refactor PDF generation to remove unnecessary labels from user info text

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -7,12 +7,12 @@ export function generatePDF() {
   // Set font size and style for the name
   doc.setFontSize(22);
   doc.setFont('helvetica', 'bold');
-  doc.text(`Name: ${userInfo.name}`, 10, 20);
+  doc.text(userInfo.name, 10, 20);
 
   // Set font size and style for the title
   doc.setFontSize(16);
   doc.setFont('helvetica', 'normal');
-  doc.text(`Title: ${userInfo.title}`, 10, 30);
+  doc.text(userInfo.title, 10, 30);
 
   // Set font size and style for the skills
   doc.setFontSize(14);
