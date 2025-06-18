18a54437f20cb91cda7a58c16e28738e139cc5e0
rniemela77 <rvniemela@hotmail.com>
2025-06-18 14:47:27 -0700

    add contact line and improve pdf styling

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -4,24 +4,55 @@ import userInfo from '../data/userInfo.json';
 export function generatePDF() {
   const doc = new jsPDF();
 
-  // Set font size and style for the name
-  doc.setFontSize(22);
+  // Constants
+  const pageWidth = doc.internal.pageSize.getWidth();
+  const pageMarginX = 10;
+  const pageMarginY = 10;
+  const lineSpacing = 5;
+
+  const nameFontSize = 30;
+  const nameYOffset = -20;
+
+  const titleFontSize = 16;
+  const titleYOffset = -9;
+
+  const contactFontSize = 12;
+  const contactYOffset = -4;
+
+  // Name
+  const nameY = pageMarginY + nameFontSize + nameYOffset;
+  doc.setFontSize(nameFontSize);
   doc.setFont('helvetica', 'bold');
-  doc.text(userInfo.name, 10, 20);
+  const nameTextWidth = doc.getTextWidth(userInfo.name);
+  doc.text(userInfo.name, (pageWidth - nameTextWidth) / 2, nameY);
 
-  // Set font size and style for the title
-  doc.setFontSize(16);
+  // Title
+  const titleY = nameY + titleFontSize + titleYOffset;
+  doc.setFontSize(titleFontSize);
   doc.setFont('helvetica', 'normal');
-  doc.text(userInfo.title, 10, 30);
+  const titleTextWidth = doc.getTextWidth(userInfo.title);
+  doc.text(userInfo.title, (pageWidth - titleTextWidth) / 2, titleY);
 
-  // Set font size and style for the skills
-  doc.setFontSize(14);
-  doc.setFont('helvetica', 'italic');
-  doc.text('Skills:', 10, 40);
-  doc.setFont('helvetica', 'normal');
-  userInfo.skills.forEach((skill, index) => {
-    doc.text(`- ${skill}`, 20, 50 + index * 10);
+  // Contact information
+  const contactY = titleY + titleFontSize + contactYOffset;
+  doc.setFontSize(contactFontSize);
+  const contactInfoArray = Object.values(userInfo.contact);
+  const contactInfo = contactInfoArray.join(' • ');
+  const maxLineWidth = pageWidth - 2 * pageMarginX;
+  const contactLines = doc.splitTextToSize(contactInfo, maxLineWidth);
+  contactLines.forEach((line, index) => {
+    const lineWidth = doc.getTextWidth(line);
+    // Remove trailing dot if present
+    if (line.endsWith(' •')) {
+      line = line.slice(0, -2);
+    }
+    doc.text(line, (pageWidth - lineWidth) / 2, contactY + index * lineSpacing);
   });
+
+  // Horizontal line
+  const contactLineY = contactY + contactLines.length * lineSpacing;
+  doc.setLineWidth(0.5);
+  doc.line(pageMarginX, contactLineY, pageWidth - pageMarginX, contactLineY);
 
   return doc.output('datauristring');
 } 
