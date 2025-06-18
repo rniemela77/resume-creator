e32c10391b4c563c5b6a9de62cc402b2d124a525
rniemela77 <rvniemela@hotmail.com>
2025-06-18 14:21:41 -0700

    put user data in json file

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -1,4 +1,5 @@
 import { jsPDF } from 'jspdf';
+import userInfo from '../data/userInfo.json';
 
 export function generatePDF() {
   const doc = new jsPDF();
@@ -6,21 +7,21 @@ export function generatePDF() {
   // Set font size and style for the name
   doc.setFontSize(22);
   doc.setFont('helvetica', 'bold');
-  doc.text('Name: John Doe', 10, 20);
+  doc.text(`Name: ${userInfo.name}`, 10, 20);
 
   // Set font size and style for the title
   doc.setFontSize(16);
   doc.setFont('helvetica', 'normal');
-  doc.text('Title: Software Engineer', 10, 30);
+  doc.text(`Title: ${userInfo.title}`, 10, 30);
 
   // Set font size and style for the skills
   doc.setFontSize(14);
   doc.setFont('helvetica', 'italic');
   doc.text('Skills:', 10, 40);
   doc.setFont('helvetica', 'normal');
-  doc.text('- JavaScript', 20, 50);
-  doc.text('- Vue.js', 20, 60);
-  doc.text('- PDF Generation', 20, 70);
+  userInfo.skills.forEach((skill, index) => {
+    doc.text(`- ${skill}`, 20, 50 + index * 10);
+  });
 
   return doc.output('datauristring');
 } 
